3ed466503e4adeaec3958b0722680899571429fc
Manraj962 <dmanraj27@gmail.com>
2025-03-20 19:10:38 -0600

    Update

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users.js');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
+
+//Setup the view engine
+app.set('view engine','ejs');
+app.set('views', './views');
+
+//Middleware to serve static files
+app.use(express.static('public'));
+app.use(userRoutes);
+
+app.listen(PORT, ()=>{
+    console.log(`Connected on port: ${PORT}`);
+});
